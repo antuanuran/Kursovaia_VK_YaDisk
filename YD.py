8059a8e9dc38f822a4c837091f6d79ec92934126
test <test@neto.com>
2022-12-01 14:42:40 +0300

    третий коммит

--- YD.py
+++ YD.py
@@ -11,25 +11,18 @@ class Load_yadisk:
 
 
     def create_folder(self, name_folder):
-        params =      {
-                        'path': name_folder
-                      }
-        response = requests.put(self.url_get_folder, headers=self.headers, params=params).json()
+        params = {
+                    'path': name_folder
+                 }
+        requests.put(self.url_get_folder, headers=self.headers, params=params)
 
 
-    def get_link_file(self, name_disk):
-        params =      {
-                        'path': name_disk,
-                        'overwrite': 'true'
-                      }
-        response = requests.get(self.url_get_file, headers=self.headers, params=params).json()
-        return response['href']
-
-
-    def upload_file_put(self, name_disk, link):
-        href = self.get_link_file(name_disk)
-        requests.put(href, link)
-
+    def upload_file_post(self, name_disk, link):
+       params = {
+                    'path': name_disk,
+                    'url': link
+                }
+       requests.post(self.url_get_file, headers=self.headers, params=params)
 
 
 if __name__ == '__main__':
@@ -39,7 +32,16 @@ if __name__ == '__main__':
     name_folder = '_Folder_disk'
     token = 'y0_AgAAAAAK6z8BAADLWwAAAADTjFk3EH-r368MQZa3Fykb1Dkgkk4YvhI'
 
+    # list_1 = [{'32.jpg': 'file_name', 'z': 'max size', '3.jpg': 'file_name', 'w': 'max size', '3_[2021-03-02_18:07:48].jpg': 'file_name', 'x': 'max size'}]
+    #
+    # list_2 = [{'2018-02-04_12:01:35': ['32.jpg', 'z',  'https://sun9-16.userapi.com/impf/MNQvl7_QKzdZDrcLfrTbG7cdyz8jiVXw6Bf7cA/RSD0QGW2b_A.jpg?size=919x1080&quality=96&sign=090c7e0ee2aaa2c080c958dce621fe3d&c_uniq_tag=zc82O6dxznZBmJOReeY2x4X048GIEJ7M_ycQWdFOrCU&type=album'],
+    #            '2020-03-11_11:18:52': ['3.jpg', 'w',   'https://sun9-39.userapi.com/impg/c858528/v858528142/11c2d2/9UT49kS8K7E.jpg?size=2388x2160&quality=96&sign=40d39d8e80ab9c09f63eac8b86dd088a&c_uniq_tag=oYZilw8v3riaHkRIdALhFtP0cZLmguuyUO8gCdqNxEM&type=album'],
+    #            '2021-03-02_18:07:48': ['3_[2021-03-02_18:07:48].jpg', 'x', 'https://sun9-52.userapi.com/impg/p7avRL55PQJ46d2rPe7oUZjZh-YoRX_-Zl-_Sg/L90FAys2UVo.jpg?size=500x500&quality=96&sign=ab510c2ad8131c896ecfe89c54dba1d9&c_uniq_tag=wAU9pR2GHEzflwYpDeXwaBD_mcO7W8EqXo_8SXjEWBU&type=album']
+    #            }]
+
+
+
     load_1 = Load_yadisk(token)
     load_1.create_folder(name_folder)
-    load_1.upload_file_put(name_disk, link_file)
+    load_1.upload_file_post(name_disk, link_file)
 
